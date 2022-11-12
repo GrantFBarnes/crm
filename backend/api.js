9e4968a982d648992933117d550b7d333925ad7c
Grant Barnes <gfbarnes78@gmail.com>
2022-11-12 08:12:11 -0600

    adding start of angular project

---
+++ backend/api.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+const authentication = require("../../home-page/backend/authentication.js");
+const main = require("./main.js");
+
+const router = express.Router();
+
+function returnSuccess(response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.write(JSON.stringify({ status: "ok" }));
+  response.end();
+}
+
+function rejectUnauthorized(response) {
+  authentication.removeTokenCookie(response);
+  response.writeHead(401, { "Content-Type": "application/json" });
+  response.write(JSON.stringify({ status: "unauthorized" }));
+  response.end();
+}
+
+function returnResponse(response, result) {
+  response.writeHead(result.statusCode, { "Content-Type": "application/json" });
+  response.write(JSON.stringify(result.data));
+  response.end();
+}
+
+function returnPromiseResponse(response, promise) {
+  promise
+    .then((result) => {
+      response.writeHead(result.statusCode, {
+        "Content-Type": "application/json",
+      });
+      response.write(JSON.stringify(result.data));
+      response.end();
+    })
+    .catch(() => {
+      response.writeHead(500, { "Content-Type": "application/json" });
+      response.write("error");
+      response.end();
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// APIs defined here
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+module.exports = router;
---
+++ backend/main.js
@@ -0,0 +1,35 @@
+const { v4: uuidv4 } = require("uuid");
+
+const database = require("./database.js");
+
+const id_regex =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+
+////////////////////////////////////////////////////////////////////////////////
+// Common
+
+function idIsValid(id) {
+  if (!id || !id_regex.test(id)) {
+    return false;
+  }
+  return true;
+}
+
+function dataIsValid(table, data) {
+  if (!data || !data.id || !id_regex.test(data.id)) {
+    return false;
+  }
+
+  return true;
+}
+
+function getIdFromData(data, field) {
+  if (data && data[field] && id_regex.test(data[field])) {
+    return data[field];
+  }
+  return "";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
